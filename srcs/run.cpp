3e029013fdd544c736942e03bb8c54aaedf027c9
Emre Şimşek <emresimsek321@gmail.com>
2023-11-18 18:10:41 +0300

    copy: UP

--- srcs/run.cpp
+++ srcs/run.cpp
@@ -40,29 +40,47 @@ int getClientCommand(Server &sv, Client &cl, std::string buffer)
     
     //if (cl.getClientAuth() == AUTH)
         //return (runCommand(sv, cl, command));
-    if (command[0] == "PASS" && command.size() == 2 && cl.getClientAuth() == NOT_AUTH)
+    if (command[0] == "PASS" && cl.getClientAuth() == NOT_AUTH)
     {
         if (sv.checkPassword(command[1]) == 0)
         {
             cl.setClientAuth(PASS_AUTH);
             cl.sendMessage(":" + std::string(sv.hostname) + " 001 " + cl.getClientName() + " :PASSWORD CORRECT\n");
         }
+        else if (command.size() != 2)
+            cl.sendMessage(":" + std::string(sv.hostname) + " 464 " + cl.getClientName() + "PASS usage 'PASS <password>'\n");
         else
             cl.sendMessage(":" + std::string(sv.hostname) + " 464 " + cl.getClientName() + " :PASSWORD INCORRECT\n");
     }
-    if (command[0] == "NICK" && command.size() == 2 && cl.getClientAuth() == PASS_AUTH)
+    if (command[0] == "NICK")
     {
-        cl.setNickName(command[1]);
-        cl.setClientAuth(NICK_AUTH);
-        cl.sendMessage(":" + std::string(sv.hostname) + " 001 " + cl.getClientName() + " :Nickname is saved\n");
+        if (cl.getClientAuth() == PASS_AUTH && command.size() == 2)
+        {
+            cl.setNickName(command[1]);
+            cl.setClientAuth(NICK_AUTH);
+            cl.sendMessage(":" + std::string(sv.hostname) + " 001 " + cl.getClientName() + " :Nickname is saved\n");
+        }
+        else if (cl.getClientAuth() == NOT_AUTH)
+            cl.sendMessage(":" + std::string(sv.hostname) + " 000 " + cl.getClientName() + " :You must enter password first!\n");
+        else
+            cl.sendMessage(":" + std::string(sv.hostname) + " 461 " + cl.getClientName() + " :NICK usage 'NICK <nick name>'\n");
     }
-    if (command[0] == "USER" && command.size() >= 4 && cl.getClientAuth() == NICK_AUTH)
+    if (command[0] == "USER")
     {
-        cl.username = command[1];
-        cl.hostname = command[2];
-        cl.servername = command[3];
-        cl.setClientAuth(AUTH);
-        cl.sendMessage(":" + std::string(sv.hostname) + " 001 " + cl.getClientName() + " :Welcome to karsimIRC\n");
+        if (cl.getClientAuth() == NICK_AUTH && command.size() >= 4)
+        {
+            cl.username = command[1];
+            cl.hostname = command[2];
+            cl.servername = command[3];
+            cl.setClientAuth(AUTH);
+            cl.sendMessage(":" + std::string(sv.hostname) + " 001 " + cl.getClientName() + " :Welcome to karsimIRC\n");
+        }
+        else if (cl.getClientAuth() == PASS_AUTH)
+            cl.sendMessage(":" + std::string(sv.hostname) + " 000 " + cl.getClientName() + " :You must enter nickname!\n");
+        else if (cl.getClientAuth() == NOT_AUTH)
+            cl.sendMessage(":" + std::string(sv.hostname) + " 000 " + cl.getClientName() + " :You must enter nickname and password!\n");
+        else
+            cl.sendMessage(":" + std::string(sv.hostname) + " 461 " + cl.getClientName() + "USER usage 'USER <user name> <host name> <serv name>'\n");
     }
     return 1;
 }
